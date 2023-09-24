c548cce81f109b357d2b006bccef610072019b28
Ernest <143043338+e05910879@users.noreply.github.com>
2023-09-23 18:59:15 -0700

    Remove operator button function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 let firstNumber, operator, secondNumber;
+let displayingUserInput = false;
 
 function operate(firstNumber, operator, secondNumber) {
     switch(operator) {
@@ -19,11 +20,10 @@ function operate(firstNumber, operator, secondNumber) {
 
 const display = document.querySelector('#display');
 
-let displayingUserInput = false;
 function numberButtonFunction(number) {
     if (displayingUserInput) {
         display.textContent += number;
-    } else {    // if display is displaying '0'
+    } else {    // if display is displaying '0' OR right after operator is pressed
         if (number !== '0') {
             display.textContent = number;
             displayingUserInput = true;
@@ -48,15 +48,13 @@ clearButton.addEventListener('click', () => {
     clearButtonFunction();
 });
 
-function operatorButtonFunction(operatorButton) {
-    //should include displayingUserInput = false
-
+function operatorButtonFunction(operator) {
 
 }
 const operatorButtons = document.querySelectorAll('.operator-button');
 operatorButtons.forEach((operatorButton) => {
     operatorButton.addEventListener('click', () => {
-        operatorButtonFunction(operatorButton);
+        operatorButtonFunction(operatorButton.id);
     });
 });
 
@@ -72,45 +70,3 @@ equalsButton.addEventListener('click', () => {
         secondNumber = Number(display.textContent);
         operate(firstNumber, operator, secondNumber);
 });
-
-
-/*
-Equals sign and operator sign should be treated as mutually exclusive functions.
-
-Equals sign:
-Equals sign only works if in an 'operator pressed' state. Otherwise, it does nothing.
-If an operator is pressed, the operator function will have already saved the preceding value into firstNumber.
-Then pressing equals will save the display value into second number, and call operate() to perform the corresponding
-operation.
-The resulting sum should then be stored in firstNumber.
-
-Operator sign:
-The calculator starts off with its 'operator-pressed' state turned off.
-
-If the operator button is FIRST pressed, i.e. the operator-pressed state is initially off, 
-the value in the screen must be passed to firstNumber, and the calculator should enter an 'operator pressed' state.
-If already in the 'operator pressed' state no modification of the display value is made, i.e. the 'newValueEntered' state is off, 
-aside from the equals button pressing different operators will merely switch the operator.
-Else if in the 'operator pressed' state the display value were to be modified, pressing the operator button 
-again would act as an equals sign, as well as calling on the operator button again.
-Exception: a + b * c.
-
-// if first time pressing operator button, pass display value to firstNum and turn on operatorIsSelected state
-if (operatorIsSelected === false) {
-    operatorIsSelected = true;
-    firstNumber = display.textContent;
-    newValueEntered = false;
-}
-else {
-    if (newValueEntered === true) {
-        equalsFunction();
-        firstNumber = display.textContent;
-
-    }
-}
-
-
-NOTE: the only buttons that can reset operatorIsSelected state are the equals button or clear button.
-
-
-*/
